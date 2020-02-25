c719aa80746bbe144e4036acbc4544901a67d711
MaksimBocharov <maksimbocharov@gmail.com>
2020-02-25 21:13:07 +0200

    first

---
+++ untitled/src/Exercise1.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(6);
+        int randomNumber2 = randomGenerator.nextInt(6);
+        int randomNumber3 = randomGenerator.nextInt(6);
+
+        System.out.println(randomNumber);
+        System.out.println(randomNumber2);
+        System.out.println(randomNumber3);
+        }
+    }
